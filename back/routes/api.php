f68987b67f287c08b599f837fdd59ff4099dcf86
Hisashi <hisasi622@gmail.com>
2021-10-16 14:59:09 +0900

    name追加

--- back/routes/api.php
+++ back/routes/api.php
@@ -21,7 +21,7 @@ Route::group(
         'prefix' => 'auth',
     ],
     function ($router) {
-        Route::get('/login', [AuthController::class, 'login']);
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/refresh', [AuthController::class, 'refresh']);
