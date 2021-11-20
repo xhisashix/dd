a5181902948bfb462f682690677f0c2eb0e99b23
Hisashi <hisasi622@gmail.com>
2021-11-20 14:27:44 +0900

    いらないので削除

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,26 +1,8 @@
-<template lang='pug'>
+<template lang="pug">
   .container
-    p 名前：{{ user.name }}
-    p 年齢：{{ user.email }}
-    el-button.button(type="info" @click="logout") ログアウト
+    h1 TOPページ
 </template>
 
 <script>
-export default {
-  middleware({ store, redirect }) {
-    if(!store.$auth.loggedIn) {
-      redirect('/login');
-    }
-  },
-  computed: {
-    user() {
-      return this.$auth.user;
-    }
-  },
-  methods: {
-    logout() {
-      this.$auth.logout();
-    },
-  }
-}
+export default {}
 </script>
